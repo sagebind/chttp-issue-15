c5fd6bc20572ee978b1108a3d6d3587ee78f64f5
Stephen M. Coakley <me@stephencoakley.com>
2018-11-05 22:21:28 -0600

    Reduce even further; libcurl does not seem to be the cause

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-extern crate curl;
 extern crate grpcio_sys;
+extern crate openssl;
 
-use curl::easy::Easy;
+use openssl::ssl::{SslMethod, SslConnector};
+use std::io::Write;
+use std::net::TcpStream;
 
 fn main() {
-    let mut easy = Easy::new();
-    easy.url("https://www.rust-lang.org/").unwrap();
-    easy.perform().unwrap();
+    // Connect using libssl.
+    let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+    let stream = TcpStream::connect("google.com:443").unwrap();
+    let mut stream = connector.connect("google.com", stream).unwrap();
+    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
 
     unsafe {
         // Not called, just present to prevent dead code elimination.
